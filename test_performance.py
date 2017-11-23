bf4a013ecd2eafcbe73d15f20f97d3790d21b2ec
Vladimir Bolshakov <vovanbo@gmail.com>
2017-11-23 15:36:02 +0300

    Hyperlink package benchmark is added
    Refactor benchmark

--- test_performance.py
+++ test_performance.py
@@ -2,9 +2,10 @@
 import pytest
 
 import furl
+import hyperlink
 import yarl
 import urlobject
-from url import URL as URLPY
+import url
 
 NON_ASCII_URL = 'http://εμπορικόσήμα.eu/путь/這裡'
 NON_ASCII_URL_ENCODED = \
@@ -14,52 +15,73 @@ NON_ASCII_URL_ENCODED = \
 @pytest.mark.benchmark(group='Creation of simple URL')
 @pytest.mark.parametrize(
     'factory',
-    [furl.furl, yarl.URL, urlobject.URLObject, URLPY.parse],
-    ids=['furl', 'yarl', 'urlobject', 'url-py']
+    [
+        furl.furl,
+        yarl.URL,
+        urlobject.URLObject,
+        url.URL.parse,
+        hyperlink.URL.from_text
+    ],
+    ids=['furl', 'yarl', 'urlobject', 'url-py', 'hyperlink']
 )
 def test_create_simple_url(benchmark, factory):
-    url = 'http://example.com/path/to/?arg1=a&arg2=b#fragment'
-    created_url = benchmark(factory, url)
-    if factory == URLPY.parse:
-        assert str(created_url.punycode().escape()) == url
-    elif factory == furl.furl:
-        assert created_url.url == url
+    uri = 'http://example.com/path/to/?arg1=a&arg2=b#fragment'
+    created_url = benchmark(factory, uri)
+    if isinstance(created_url, url.URL):
+        assert str(created_url.punycode().escape()) == uri
+    elif isinstance(created_url, furl.furl):
+        assert created_url.url == uri
+    elif isinstance(created_url, hyperlink.URL):
+        assert created_url.to_uri().to_text() == uri
     else:
-        assert str(created_url) == url
+        assert str(created_url) == uri
 
 
 @pytest.mark.benchmark(group='Creation of non-ASCII URL')
 @pytest.mark.parametrize(
     'factory',
-    [furl.furl, yarl.URL, urlobject.URLObject.from_iri, URLPY.parse],
-    ids=['furl', 'yarl', 'urlobject', 'url-py']
+    [
+        furl.furl,
+        yarl.URL,
+        urlobject.URLObject.from_iri,
+        url.URL.parse,
+        hyperlink.URL.from_text
+    ],
+    ids=['furl', 'yarl', 'urlobject', 'url-py', 'hyperlink']
 )
 def test_create_non_ascii_url(benchmark, factory):
     created_url = benchmark(factory, NON_ASCII_URL)
-    if factory == URLPY.parse:
+    if isinstance(created_url, url.URL):
         assert str(created_url.punycode().escape()) == NON_ASCII_URL_ENCODED
-    elif factory == furl.furl:
+    elif isinstance(created_url, furl.furl):
         assert created_url.url == NON_ASCII_URL_ENCODED
+    elif isinstance(created_url, hyperlink.URL):
+        assert created_url.to_uri().to_text() == NON_ASCII_URL_ENCODED
     else:
         assert str(created_url) == NON_ASCII_URL_ENCODED
 
 
 @pytest.mark.benchmark(group='Conversion of URL to string')
 @pytest.mark.parametrize(
-    'url',
-    [furl.furl(NON_ASCII_URL),
-     yarl.URL(NON_ASCII_URL),
-     urlobject.URLObject.from_iri(NON_ASCII_URL),
-     URLPY.parse(NON_ASCII_URL)],
-    ids=['furl', 'yarl', 'urlobject', 'url-py']
+    'created_url',
+    [
+        furl.furl(NON_ASCII_URL),
+        yarl.URL(NON_ASCII_URL),
+        urlobject.URLObject.from_iri(NON_ASCII_URL),
+        url.URL.parse(NON_ASCII_URL),
+        hyperlink.URL.from_text(NON_ASCII_URL)
+    ],
+    ids=['furl', 'yarl', 'urlobject', 'url-py', 'hyperlink']
 )
-def test_url_to_string(benchmark, url):
-    if isinstance(url, URLPY):
-        result = benchmark(lambda: url.punycode().escape())
-    elif isinstance(url, furl.furl):
-        result = benchmark(url.tostr)
+def test_url_to_string(benchmark, created_url):
+    if isinstance(created_url, url.URL):
+        result = benchmark(lambda: created_url.punycode().escape())
+    elif isinstance(created_url, furl.furl):
+        result = benchmark(created_url.tostr)
+    elif isinstance(created_url, hyperlink.URL):
+        result = benchmark(lambda: created_url.to_uri().to_text())
     else:
-        result = benchmark(str, url)
+        result = benchmark(str, created_url)
 
     assert result == NON_ASCII_URL_ENCODED
 
